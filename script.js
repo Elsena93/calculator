3fa3a0cc733d83b29e948de1ec5c6fc062b79520
Elsena93 <elsena.kurniadi@gmail.com>
2022-09-21 10:15:55 +0700

    refactor: decouple screen.textContent with current value

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //Put var as current screen value
 const screen = document.querySelector('.screen');
 
+//var to store screen value
+let screenValue = "";
+
 //Var to store operands
 let operand1 = false;
 let operand2 = false;
@@ -18,18 +21,21 @@ function updateScreen(item) {
     if (item.target.classList.contains('num')) {
         
         //Checking condition if its time to fill operand2
-        if ((operand1 != false || operand1 === "0") && operand2filling == false) {
-            screen.textContent = "";
+        if ((operand1 != false || operand1 === "0") && operand2filling == false && operator != false) {
+            screenValue = "";
+            screen.textContent = screenValue;
             //Turn switch for defining operand2 so screen not resetted when user input operand2
             operand2filling = true;
         }
 
-        screen.textContent += (item.target.dataset.key);
+        screenValue += (item.target.dataset.key);
+        screen.textContent = screenValue;
     }
 
     //Update for ac buttons
     if (item.target.classList.contains('ac')) {
-        screen.textContent = "";
+        screenValue = "";
+        screen.textContent = screenValue;
         operand1 = false;
         operand2 = false;
         operator = false; 
@@ -37,27 +43,33 @@ function updateScreen(item) {
 
     //Update for plus minus
     if (item.target.classList.contains('plus-minus')) {
-        if (screen.textContent[0] != "-") {
-            screen.textContent = "-" + screen.textContent;
+        if (screenValue[0] != "-") {
+            screenValue = "-" + screenValue;
+            screen.textContent = screenValue;
         }
 
-        else if (screen.textContent[0] == "-") {
-            screen.textContent = screen.textContent.substring(1);
+        else if (screenValue[0] == "-") {
+            screenValue = screenValue.substring(1);
+            screen.textContent = screenValue;
         }
     }
     
     //Update for .
     if (item.target.classList.contains('dot')) {
-        if (screen.textContent.substring(0, screen.textContent.length-1).includes(".")) {
+
+        //Check if already dot there in middle of number
+        if (screenValue.substring(0, screenValue.length-1).includes(".")) {
             return
         }
         
-        else if (screen.textContent[screen.textContent.length - 1] != ".") {
-            screen.textContent = screen.textContent +  ".";
+        else if (screenValue[screenValue.length - 1] != ".") {
+            screenValue = screenValue +  ".";
+            screen.textContent = screenValue;
         }
 
-        else if (screen.textContent[screen.textContent.length - 1] == ".") {
-            screen.textContent = screen.textContent.substring(0, screen.textContent.length-1);
+        else if (screenValue[screenValue.length - 1] == ".") {
+            screenValue = screenValue.substring(0, screenValue.length-1);
+            screen.textContent = screenValue;
         }
     }
 
@@ -67,13 +79,14 @@ function updateScreen(item) {
         if ((operand1 != false || operand1 === "0") &&
         operator != false && operand2filling == true) {
 
-            operand2 = screen.textContent;
+            operand2 = screenValue;
 
             //Executing operate()
-            screen.textContent = operate(operator, operand1, operand2);
+            screenValue = operate(operator, operand1, operand2);
+            screen.textContent = screenValue;
 
             //resetting variables
-            operand1 = screen.textContent; //operand1 values equal to last operation  result
+            operand1 = screenValue; //operand1 values equal to last operation  result
             operand2 = false;
             operand2filling = false;
             operator = false;
@@ -84,28 +97,30 @@ function updateScreen(item) {
     if (item.target.classList.contains('operand')) {
         
         //Checking if screen is empty so no value could be stored
-        if (screen.textContent == "") {
+        if (screenValue == "") {
             console.log("please, input")
         }
         
         //Storing operand1 for the first time
         else if (operand1 == false && operand1 !== "0") {
-            operand1 = screen.textContent;
+            operand1 = screenValue;
             operator = item.target.textContent;
         }
         else if ((operand1 != false || operand1 === "0") && (operand2 == false && operand2 !== "0")) {
             
             if (operand2filling!= false) {
-                operand2 = screen.textContent;
-                screen.textContent = "";
+                operand2 = screenValue;
+                screenValue = "";
+                screen.textContent = screenValue;
             }
             
             if ((operand2 != false || operand2 === "0") && operand2filling != false) {
                 //Executing operate()
-                screen.textContent = operate(operator, operand1, operand2);
+                screenValue = operate(operator, operand1, operand2);
+                screen.textContent = screenValue;
 
                 //resetting variables
-                operand1 = screen.textContent; //operand1 values equal to last operation  result
+                operand1 = screenValue; //operand1 values equal to last operation  result
                 operand2 = false;
                 operand2filling = false;
             }
